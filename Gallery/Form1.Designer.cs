248fb8a73fea1bc8bbfc491a0d68c393de493115
kakakepi <viklybi1@gmail.com>
2025-03-21 23:37:15 +0300

    Added all buttons work

--- Gallery/Form1.Designer.cs
+++ Gallery/Form1.Designer.cs
@@ -4,12 +4,23 @@ namespace Gallery
 {
     public class ImageMember
     {
-        string name;
-        string path;
+        public string name;
+        public string path;
+        public string title;
         public ImageMember(string name, string path)
         {
             this.name = name;
             this.path = path;
+        }
+        public ImageMember(string name, string path, string title)
+        {
+            this.name = name;
+            this.path = path;
+            this.title = title;
+        }
+        public override string ToString()
+        {
+           return name;
         }
     }
     partial class Gallery
@@ -51,6 +62,8 @@ namespace Gallery
             SaverTXT = new SaveFileDialog();
             OpenFileIMG = new Button();
             DeleteIMG = new Button();
+            ClearListBox = new Button();
+            openerIMG = new OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)ShowIMG).BeginInit();
             ((System.ComponentModel.ISupportInitialize)Russia).BeginInit();
             SuspendLayout();
@@ -59,7 +72,7 @@ namespace Gallery
             // 
             OpenTXT.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             OpenTXT.AutoSize = true;
-            OpenTXT.Location = new Point(1458, 12);
+            OpenTXT.Location = new Point(1458, 60);
             OpenTXT.Name = "OpenTXT";
             OpenTXT.Size = new Size(424, 42);
             OpenTXT.TabIndex = 0;
@@ -70,9 +83,9 @@ namespace Gallery
             // SaveIMG
             // 
             SaveIMG.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            SaveIMG.Location = new Point(1458, 819);
+            SaveIMG.Location = new Point(1458, 881);
             SaveIMG.Name = "SaveIMG";
-            SaveIMG.Size = new Size(179, 178);
+            SaveIMG.Size = new Size(179, 116);
             SaveIMG.TabIndex = 1;
             SaveIMG.Text = "Сохранить .txt файл с изображениями";
             SaveIMG.UseVisualStyleBackColor = true;
@@ -81,17 +94,18 @@ namespace Gallery
             // ListIMG
             // 
             ListIMG.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            ListIMG.DisplayMember = "name";
             ListIMG.FormattingEnabled = true;
             ListIMG.Location = new Point(1458, 108);
             ListIMG.Name = "ListIMG";
-            ListIMG.Size = new Size(424, 612);
+            ListIMG.Size = new Size(424, 580);
             ListIMG.TabIndex = 2;
             ListIMG.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // NameIMG
             // 
             NameIMG.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            NameIMG.Location = new Point(1458, 774);
+            NameIMG.Location = new Point(1458, 694);
             NameIMG.Name = "NameIMG";
             NameIMG.Size = new Size(424, 39);
             NameIMG.TabIndex = 3;
@@ -111,50 +125,66 @@ namespace Gallery
             // 
             Russia.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             Russia.Image = (Image)resources.GetObject("Russia.Image");
-            Russia.Location = new Point(1643, 819);
+            Russia.Location = new Point(1643, 787);
             Russia.Name = "Russia";
-            Russia.Size = new Size(239, 178);
+            Russia.Size = new Size(239, 210);
             Russia.SizeMode = PictureBoxSizeMode.StretchImage;
             Russia.TabIndex = 6;
             Russia.TabStop = false;
             // 
             // OpenerTXT
             // 
-            OpenerTXT.FileName = "Выберите .txt файл с ссылками на изображения";
-            OpenerTXT.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            OpenerTXT.Filter = "Text files(*.txt)|*.txt";
             // 
             // SaverTXT
             // 
-            SaverTXT.FileName = "Сохраните .txt файл с изображениями";
-            SaverTXT.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            SaverTXT.Filter = "Text files(*.txt)|*.txt";
             // 
             // OpenFileIMG
             // 
             OpenFileIMG.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             OpenFileIMG.AutoSize = true;
-            OpenFileIMG.Location = new Point(1458, 60);
+            OpenFileIMG.Location = new Point(1458, 12);
             OpenFileIMG.Name = "OpenFileIMG";
             OpenFileIMG.Size = new Size(424, 42);
             OpenFileIMG.TabIndex = 7;
             OpenFileIMG.Text = "Открыть файл изображения";
             OpenFileIMG.UseVisualStyleBackColor = true;
+            OpenFileIMG.Click += OpenFileIM_Click;
             // 
             // DeleteIMG
             // 
             DeleteIMG.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
             DeleteIMG.AutoSize = true;
-            DeleteIMG.Location = new Point(1458, 726);
+            DeleteIMG.Location = new Point(1458, 739);
             DeleteIMG.Name = "DeleteIMG";
             DeleteIMG.Size = new Size(424, 42);
             DeleteIMG.TabIndex = 9;
             DeleteIMG.Text = "Удалить изображение из списка";
             DeleteIMG.UseVisualStyleBackColor = true;
+            DeleteIMG.Click += Delete_Click;
+            // 
+            // ClearListBox
+            // 
+            ClearListBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            ClearListBox.Location = new Point(1458, 787);
+            ClearListBox.Name = "ClearListBox";
+            ClearListBox.Size = new Size(179, 88);
+            ClearListBox.TabIndex = 10;
+            ClearListBox.Text = "Очистить список";
+            ClearListBox.UseVisualStyleBackColor = true;
+            ClearListBox.Click += Clear_Click_1;
+            // 
+            // openerIMG
+            // 
+            openerIMG.Filter = "Image Files (*.bmp; *.jpg; *.jpeg; *.png; *.gif; *.tiff)|*.bmp;*.jpg;*.jpeg;*.png;*.gif;*.tiff";
             // 
             // Gallery
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1894, 1009);
+            Controls.Add(ClearListBox);
             Controls.Add(DeleteIMG);
             Controls.Add(OpenFileIMG);
             Controls.Add(Russia);
@@ -184,5 +214,7 @@ namespace Gallery
         private SaveFileDialog SaverTXT;
         private Button OpenFileIMG;
         private Button DeleteIMG;
+        private Button ClearListBox;
+        private OpenFileDialog openerIMG;
     }
 }
--- Gallery/Form1.cs
+++ Gallery/Form1.cs
@@ -13,25 +13,57 @@ namespace Gallery
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            ImageMember? image = ListIMG.SelectedItem as ImageMember;
+            if (image != null && File.Exists(image.path))
+            {
+                ShowIMG.Image = Image.FromFile(image.path);
+                NameIMG.Text = image.title;
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            if (ListIMG.SelectedItem != null)
+            {
+                ImageMember? image = ListIMG.SelectedItem as ImageMember;
+                image.title = NameIMG.Text;
+            }
         }
         private void OpenTXT_Click(object sender, EventArgs e)
         {
             if (OpenerTXT.ShowDialog() == DialogResult.Cancel)
                 return;
+
             string filename = OpenerTXT.FileName;
             string fileText = System.IO.File.ReadAllText(filename);
-            foreach (string line in fileText.Split("\n"))
+
+            foreach (string line in fileText.Split('\n'))
             {
-                ListIMG.Items.Add($"{line}");
+                string[] imageMembers = line.Split(',');
+                if (imageMembers.Length >= 2)
+                {
+                    string[] imageName1 = imageMembers[0].Split('\\');
+                    string imageName = imageName1[imageName1.Length - 1];
+                    if (!string.IsNullOrEmpty(imageMembers[0]))
+                    {
+                        ImageMember imageMember = new ImageMember(imageName, imageMembers[0], imageMembers[1]);
+                        ListIMG.Items.Add(imageMember);
+                    }
+                }
+                if (imageMembers.Length == 1)
+                {
+                    string[] imageName1 = imageMembers[0].Split('\\');
+                    string imageName = imageName1[imageName1.Length - 1];
+                    if (!string.IsNullOrEmpty(imageMembers[0]))
+                    {
+                        ImageMember imageMember = new ImageMember(imageName, imageMembers[0]);
+                        ListIMG.Items.Add(imageMember);
+                    }
+                }
             }
             MessageBox.Show("Файл открыт");
         }
+
         private void SaveIMG_Click(object sender, EventArgs e)
         {
             if (SaverTXT.ShowDialog() == DialogResult.Cancel)
@@ -40,15 +72,44 @@ namespace Gallery
             string[] lines = new string[ListIMG.Items.Count];
             for (int i = 0; i < ListIMG.Items.Count; i++)
             {
-                
+                var image = ListIMG.Items[i] as ImageMember;
+                if (image != null)
+                {
+                    lines[i] = string.IsNullOrEmpty(image.title) ? image.path : $"{image.path},{image.title}";
+                }
             }
-            File.WriteAllLines(.FileName, lines);
+            System.IO.File.WriteAllText(filename, string.Join("\n", lines));
             MessageBox.Show("Файл сохранен");
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Delete_Click(object sender, EventArgs e)
         {
+            ListIMG.Items.Remove(ListIMG.SelectedItem);
+            ShowIMG.Image = null;
+            ListIMG.SelectedIndex = -1;
+            NameIMG.Text = null;
+        }
 
+        private void Clear_Click_1(object sender, EventArgs e)
+        {
+            ListIMG.Items.Clear();
+            ShowIMG.Image = null;
+            ListIMG.SelectedIndex = -1;
+            NameIMG.Text = null;
+        }
+        private void OpenFileIM_Click(object sender, EventArgs e)
+        {
+            if (openerIMG.ShowDialog() == DialogResult.Cancel)
+                return;
+            string imagePath = openerIMG.FileName;
+            string[] imageName1 = imagePath.Split('\\');
+            string imageName = imageName1[imageName1.Length - 1];
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                ImageMember imageMember = new ImageMember(imageName, imagePath);
+                ListIMG.Items.Add(imageMember);
+            }
+            MessageBox.Show("Файл открыт");
         }
     }
 }
